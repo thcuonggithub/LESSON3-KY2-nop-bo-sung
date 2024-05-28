e6b0e5c2fc190946e59c3ee23174a57b4751e656
thcuonggithub <166672916+thcuonggithub@users.noreply.github.com>
2024-05-29 04:23:12 +0700

    Add files via upload

---
+++ Card.jsx
@@ -0,0 +1,145 @@
+const Card = () => {
+    return <>
+    <div className = "Wrapper">
+    <ul className="product"> 
+        <li>
+            <div className="card">
+                <img src="src/assets/001.png" alt="" />
+                <p className="pokemon_id">#0001</p>
+                <p className="pokemon_name">Bulbasaur</p>
+                <div className="pokemon_class_wrapper">
+                    <span id="Gra">Grass</span>
+                    <span id="Poi">Poison</span>
+                </div>
+            </div>
+        </li>
+
+        <li>
+            <div className="card">
+            <img src="src/assets/002.png" alt="" />
+            <p className="pokemon_id">#0002</p>
+            <p className="pokemon_name">Ivysaur</p>
+            <div className="pokemon_class_wrapper">
+                <span id="Gra">Grass</span>
+                <span id="Poi">Poison</span>
+            </div>
+            </div>
+        </li>
+
+        <li>
+            <div className="card">
+            <img src="src/assets/003.png" alt="" />
+            <p className="pokemon_id">#0003</p>
+            <p className="pokemon_name">Venusaur</p>
+            <div className="pokemon_class_wrapper">
+                <span id="Gra">Grass</span>
+                <span id="Poi">Poison</span>
+            </div>
+            </div>
+        </li>
+
+        <li>
+            <div className="card">
+            <img src="src/assets/004.png" alt="" />
+            <p className="pokemon_id">#0004</p>
+            <p className="pokemon_name">Charmander</p>
+            <div className="pokemon_class_wrapper">
+                <span id="Fi">Fire</span>
+            </div>
+            </div>
+        </li>
+
+        <li>
+            <div className="card">
+            <img src="src/assets/005.png" alt="" />
+            <p className="pokemon_id">#0005</p>
+            <p className="pokemon_name">Charmeleon</p>
+            <div className="pokemon_class_wrapper">
+                <span id="Fi">Fire</span>
+            </div>
+            </div>
+        </li>
+
+        <li>
+            <div className="card">
+            <img src="src/assets/006.png" alt="" />
+            <p className="pokemon_id">#0006</p>
+            <p className="pokemon_name">Charizard</p>
+            <div className="pokemon_class_wrapper">
+                <span id="Fi">Fire</span>
+                <span id="Fly">Flying</span>
+            </div>
+            </div>
+        </li>
+
+        <li>
+            <div className="card">
+            <img src="src/assets/007.png" alt="" />
+            <p className="pokemon_id">#0007</p>
+            <p className="pokemon_name">Squirtle</p>
+            <div className="pokemon_class_wrapper">
+                <span id="Wat">Water</span>
+            </div>
+            </div>
+        </li>
+
+        <li>
+            <div className="card">
+            <img src="src/assets/008.png" alt="" />
+            <p className="pokemon_id">#0008</p>
+            <p className="pokemon_name">Wartortle</p>
+            <div className="pokemon_class_wrapper">
+                <span id="Wat">Water</span>
+            </div>
+            </div>
+        </li>
+
+        <li>
+            <div className="card">
+            <img src="src/assets/009.png" alt="" />
+            <p className="pokemon_id">#0009</p>
+            <p className="pokemon_name">Blastoise</p>
+            <div className="pokemon_class_wrapper">
+                <span id="Wat">Water</span>
+            </div>
+            </div>  
+        </li>
+
+        <li>
+            <div className="card">
+            <img src="src/assets/010.png" alt="" />
+            <p className="pokemon_id">#0010</p>
+            <p className="pokemon_name">Caterpie</p>
+            <div className="pokemon_class_wrapper">
+                <span id="bug">Bug</span>
+            </div>
+            </div>
+        </li>
+            
+        <li>
+            <div className="card">
+            <img src="src/assets/011.png" alt="" />
+            <p className="pokemon_id">#0011</p>
+            <p className="pokemon_name">Metapod</p>
+            <div className="pokemon_class_wrapper">
+                <span id="bug">Bug</span>
+            </div>
+            </div>   
+        </li>
+
+        <li>
+            <div className="card">
+            <img src="src/assets/012.png" alt="" />
+            <p className="pokemon_id">#0012</p>
+            <p className="pokemon_name">Butterfree</p>
+            <div className="pokemon_class_wrapper">
+                <span id="bug">Bug</span>
+                <span id="Fly">Flying</span>
+            </div>
+            </div>   
+        </li> 
+    </ul>
+    </div>    
+    </>
+}
+export default Card
